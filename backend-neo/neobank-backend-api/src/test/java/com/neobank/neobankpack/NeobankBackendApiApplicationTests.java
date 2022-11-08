3725b08fab8bdb659fdfd4a50a2bdb5de373944e
PSKK-Git <saikarthik8106@gmail.com>
2022-11-08 14:08:57 +0000

    initial-1

---
+++ backend-neo/neobank-backend-api/src/test/java/com/neobank/neobankpack/NeobankBackendApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neobank.neobankpack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeobankBackendApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
